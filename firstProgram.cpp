50fc18ee1193221d02b84b19166a9fb5fac298d4
RicardoCamargoPS <ricardocamargoprado@hotmail.com>
2024-01-02 13:28:38 -0300

    teste2

--- firstProgram.cpp
+++ firstProgram.cpp
@@ -6,8 +6,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Welcome \n";
+
+    int num1, num2, sum;
+
+    cout << "Entre com o primeiro numero \n";
+    cin >> num1;
+
+    cout << "Entre com o sugundo numero \n";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "A soma é "  <<  sum << endl;
+
     cout << "Hello world  teste";    
     return 0;
-    
+
 }

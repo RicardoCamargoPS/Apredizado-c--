434248eaf0bef253136b02c0caa45c6d0170a9d6
RicardoCamargoPS <ricardocamargoprado@hotmail.com>
2024-01-04 10:10:49 -0300

    estrutura while2

--- firstProgram.cpp
+++ firstProgram.cpp
@@ -1,28 +1,34 @@
 
 #include <iostream>
+#include <iomanip>
 
 
 using namespace std;
 
 int main()
 {
-
-    int counter, grade, total, avarage;
+    float avarage;
+    int counter, grade, total;
 
     total = 0;
-    counter = 1;
+    counter = 0;
 
-    while (counter <= 10){
+    cout << "Entre grade , -1 para o fim ";
+    cin >> grade;
 
-        cout << " Enter grade: ";
-        cin >> grade;
+    while(grade != -1){
         total = total + grade;
         counter = counter + 1;
+        cout  << "Enter grade,  -1  para o fim: ";
+        cin >> grade;
     }
 
-    avarage = total / 10;
-    cout << "Class avarage is " << avarage << endl;
+    if(counter != 0 ){
+        avarage = (float) total / counter;
+        cout << "Class avarage is " << setprecision(2) << setiosflags(ios::fixed | ios::showpoint) << avarage << endl; 
+    }
+    else
+        cout << "No grades  were entered" << endl;
 
     return 0;
-
 }
